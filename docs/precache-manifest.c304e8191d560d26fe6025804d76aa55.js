46df6636cc7c262f46992cd947104c52ef55ef8a
maxwellharon <maxwellharon54@gmail.com>
2020-02-03 00:22:05 +0300

    Init

---
+++ docs/precache-manifest.c304e8191d560d26fe6025804d76aa55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-crypto-dashboard/robots.txt"
+  },
+  {
+    "revision": "78916b79665f48e190a9",
+    "url": "/vue-crypto-dashboard/js/chunk-vendors.js"
+  },
+  {
+    "revision": "017a8c39314f1e3c2680",
+    "url": "/vue-crypto-dashboard/js/chunk-1ba81c50.js"
+  },
+  {
+    "revision": "6230442eed0a890f82ff",
+    "url": "/vue-crypto-dashboard/js/chunk-1a68e4f2.js"
+  },
+  {
+    "revision": "23e2284c0e6b36a1bfa9",
+    "url": "/vue-crypto-dashboard/js/app.js"
+  },
+  {
+    "revision": "f61ad4b0ed28c286c8635224e6b6e0da",
+    "url": "/vue-crypto-dashboard/index.html"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/vue-crypto-dashboard/img/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "23e2284c0e6b36a1bfa9",
+    "url": "/vue-crypto-dashboard/css/app.css"
+  }
+];
